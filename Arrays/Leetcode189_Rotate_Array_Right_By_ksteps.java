7e49156e994050248cbaa673f12de8291fca0931
Basantham Rohit <basanthamrohit2104@gmail.com>
2026-04-15 14:36:07 +0530

    rotate array, reverse array

---
+++ Arrays/Leetcode189_Rotate_Array_Right_By_ksteps.java
@@ -0,0 +1,41 @@
+
+class Solution{
+    public void rotate(int[] nums, int k) {
+
+        int n =nums.length;
+        k =k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+
+    }
+
+    public void reverse(int nums[], int start, int end){
+        while(start<end){
+                int temp;
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+
+                start++;
+                end--;
+
+            }
+        }
+}
+
+class Leetcode189_Rotate_Array_Right_By_ksteps{
+    public static void main(String args[]){
+        Solution sol = new Solution();
+
+        int[] nums = {1,2,3,4,5,6,7};
+        int k =3;
+        sol.rotate(nums,k);
+
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+
+    }
+}
+
---
+++ Arrays/RotateArray_Reverse.java
@@ -0,0 +1,29 @@
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+
+        int start = 0;
+        int end = nums.length-1;
+        
+            while(start<end){
+                int temp;
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                
+                start++;
+                end--;
+                
+            }
+            
+            for(int i=0;i<nums.length;i++){
+                System.out.print(nums[i]+" ");
+            }
+            
+        
+        
+    }
+}
+
+
